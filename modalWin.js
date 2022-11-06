7bfe5aed8ca887115cef760ad3cf2db526bdc2fb
Dima Vasylenko <vasilenkodima292@gmail.com>
2022-11-06 21:34:13 +0200

    fix some bugs in formatting bar replace js-animations with css-animations small design changes add the ability to insert html code

--- modalWin.js
+++ modalWin.js
@@ -202,13 +202,6 @@ export class ModalWin {
 
     this.winContent.append(entryField);
 
-    // add animations on mouse events
-    let anim = new MouseEventAnimation(entryField);
-    anim.onMouseOver();
-    anim.onMouseOut();
-    anim.onFocus();
-    anim.onBlur();
-
     this.changeFontSize(entryField, index);
 
     return entryField;
@@ -218,6 +211,7 @@ export class ModalWin {
   createButton(text, background, textColor, newContainer, index) {
     if (newContainer || !this.buttonsContainer) {
       let buttonsContainer = document.createElement('div');
+      
       buttonsContainer.style.marginBottom = '5%';
       this.winContent.append(buttonsContainer);
       this.buttonsContainer = buttonsContainer;
@@ -270,177 +264,4 @@ export class ModalWin {
   align() {
     this.OnResize();
   }
-}
-
-export class MouseEventAnimation {
-  constructor(elem, startDelay=0, delay=25, minOpacity=0.04, maxOpacity=0.07) {
-    this.minOpacity = this.opacityIndex = minOpacity;
-    this.maxOpacity = maxOpacity;
-    this.focused = false;
-
-    this.elem = elem;
-    this.delay = delay;
-    this.startDelay = startDelay;
-
-    let color = getComputedStyle(this.elem).backgroundColor;
-    color = color.slice(color.indexOf('(') + 1, color.length - 1);
-    color = color.split(', ');
-    this.color = color;
-  }
-
-  onMouseOver() {
-    let elem = this.elem;
-    
-    elem.onmouseover = () => {
-      if (!this.focused) {
-        clearTimeout(this.startTimerIdOut);
-        clearInterval(this.timerIdOut);
-        
-        let color = getComputedStyle(elem).backgroundColor;
-        color = color.slice(color.indexOf('(') + 1, color.length - 1);
-        color = color.split(', ');
-        if (color.length == 3) color.push(this.minOpacity);
-  
-        this.opacityIndex = +color[3];
-
-        this.startTimerIdOver = setTimeout(() => {
-          this.timerIdOver = setInterval(() => {
-            if (this.opacityIndex >= this.maxOpacity) {
-              clearInterval(this.timerIdOver);
-            }
-            elem.style.backgroundColor = `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${this.opacityIndex})`;
-            this.opacityIndex += 0.01;
-          }, this.delay);
-        }, this.startDelay);
-      }
-    };
-  }
-
-  onMouseOut() {
-    let elem = this.elem;
-
-    elem.onmouseout = () => {
-      if (!this.focused) {
-        clearTimeout(this.startTimerIdOver);
-        clearInterval(this.timerIdOver);
-        
-        let color = getComputedStyle(elem).backgroundColor;
-        color = color.slice(color.indexOf('(') + 1, color.length - 1);
-        color = color.split(', ');
-        if (color.length == 3) color.push(this.maxOpacity);
-
-        this.opacityIndex = +color[3];
-
-        this.startTimerIdOut = setTimeout(() => {
-          this.timerIdOut = setInterval(() => {
-            if (this.opacityIndex <= this.minOpacity) {
-              clearInterval(this.timerIdOut);
-            }
-            elem.style.backgroundColor = `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${this.opacityIndex})`;
-            this.opacityIndex -= 0.01;
-          }, this.delay);
-        }, this.startDelay);
-      };
-    }
-  }
-
-  onMouseDown(startDelay=10, delay=25, addOpacity=0.05) {
-    let elem = this.elem;
-    
-    elem.onmousedown = () => {
-      if (!this.focused) {
-        clearTimeout(this.startTimerIdOver);
-        clearInterval(this.timerIdOver);
-        clearTimeout(this.startTimerIdOver);
-        clearInterval(this.timerIdOver);
-
-        clearTimeout(this.startTimerIdMouseUp);
-        clearInterval(this.timerIdMouseUp);
-
-        let color = getComputedStyle(elem).backgroundColor;
-        color = color.slice(color.indexOf('(') + 1, color.length - 1);
-        color = color.split(', ');
-        if (color.length == 3) color.push(this.maxOpacity);
-
-        this.opacityIndex = +color[3];
-        let maxOpacity = this.maxOpacity + addOpacity;
-
-        this.startTimerIdMouseDown = setTimeout(() => {
-          this.timerIdMouseDown = setInterval(() => {
-            if (this.opacityIndex >= maxOpacity) {
-              clearInterval(this.timerIdMouseDown);
-            }
-            elem.style.backgroundColor = `rgba(${this.color[0]}, ${this.color[1]}, ${this.color[2]}, ${this.opacityIndex})`;
-
-            this.opacityIndex += 0.01;
-
-          }, delay);
-        }, startDelay);
-      };
-    }
-  }
-
-  onMouseUp(startDelay=10, delay=25) {
-    let elem = this.elem;
-    
-    elem.onmouseup = () => {
-      if (!this.focused) {
-        clearTimeout(this.startTimerIdOver);
-        clearInterval(this.timerIdOver);
-        clearTimeout(this.startTimerIdOver);
-        clearInterval(this.timerIdOver);
-
-        clearTimeout(this.startTimerIdMouseDown);
-        clearInterval(this.timerIdMouseDown);
-
-        let color = getComputedStyle(elem).backgroundColor;
-        color = color.slice(color.indexOf('(') + 1, color.length - 1);
-        color = color.split(', ');
-        if (color.length == 3) color.push(this.maxOpacity);
-
-        this.opacityIndex = +color[3];
-
-        this.startTimerIdMouseUp = setTimeout(() => {
-          this.timerIdMouseUp = setInterval(() => {
-            if (this.maxOpacity >= this.opacityIndex + 0.001) {
-              clearInterval(this.timerIdMouseUp);
-            }
-            elem.style.backgroundColor = `rgba(${this.color[0]}, ${this.color[1]}, ${this.color[2]}, ${this.opacityIndex})`;
-
-            this.opacityIndex -= 0.01;
-
-          }, delay);
-        }, startDelay);
-      };
-    }
-  }
-
-  onFocus(style=false) {
-    let elem = this.elem;
-    
-    elem.onfocus = () => {
-      this.focused = true;
-      clearTimeout(this.startTimerIdOver);
-      clearTimeout(this.startTimerIdOut);
-      clearInterval(this.timerIdOver);
-      clearInterval(this.timerIdOut);
-
-      if (style) {
-        for (let prop of Object.keys(style)) {
-          elem.style[prop] = style[prop];
-        }
-      } else {
-        elem.style.backgroundColor = `rgba(${this.color[0]}, ${this.color[1]}, ${this.color[2]}, 0.1)`;
-      };
-    }
-  }
-
-  onBlur() {
-    let elem = this.elem;
-
-    elem.onblur = () => {
-      this.focused = false;
-      elem.style.backgroundColor = `rgba(${this.color[0]}, ${this.color[1]}, ${this.color[2]}, ${this.minOpacity})`;
-    }
-  };
 }
--- script.js
+++ script.js
@@ -1,34 +1,14 @@
-import {ModalWin, MouseEventAnimation} from './modalWin.js';
+import {ModalWin} from './modalWin.js';
 
 let elemCounter = 0;
 let elements = {};
 
-
-let anim = new MouseEventAnimation(document.getElementById('search'), 40, 90);
-
-anim.onMouseOver();
-anim.onMouseOut();
-anim.onFocus();
-anim.onBlur();
 
 function createElement(head, text, url) {
   let note = document.createElement('div');
 
   note.classList.add('notes');
 
-}
-
-
-let search = document.querySelectorAll('.search i');
-
-for (let el of search) {
-  let anim = new MouseEventAnimation(el, 20, 25, 0, 0.08);
-
-  anim.onMouseOver();
-  anim.onMouseOut();
-
-  anim.onMouseDown();
-  anim.onMouseUp();
 }
 
 let cancel = document.getElementById('cancel');
@@ -50,26 +30,23 @@ createElem.onclick = function() {
   let textArea = document.createElement('div');
   
   textArea.contentEditable = true;
-  textArea.classList.add('textarea');
-
-  textArea.style.marginBottom = '0px';
-  textArea.style.maxHeight = '200px';
-  textArea.style.overflow = 'auto';
-  textArea.classList.add('pretty-scroll');
+  textArea.className = 'textarea pretty-scroll';
 
   // add placeholder
-  const placeholder = 'Заметка...';
+  const placeholder = '<span id="placeholder">Заметка...</span>';
 
-  textArea.textContent === '' && (textArea.textContent = placeholder);
+  textArea.innerHTML === '' && (textArea.innerHTML = placeholder);
 
   textArea.addEventListener('focus', function (event) {
-      const value = event.target.textContent;
-      value === placeholder && (event.target.textContent = '');
+    const value = event.target.innerHTML;
+
+    value === placeholder && (event.target.innerHTML = '');
   });
 
   textArea.addEventListener('blur', function (event) {
-      const value = event.target.textContent;
-      value === '' && (event.target.textContent = placeholder);
+    const value = event.target.innerHTML;
+
+    value === '' && (event.target.innerHTML = placeholder);
   });
 
   noteData.append(textArea);
@@ -82,13 +59,13 @@ createElem.onclick = function() {
   `;
 
   let formatBarFuncs = new FormatBar(textArea, 'black', 'black');
-  let funcNames = ['bold', 'italic', 'underline', 'link', 'letterColor', 'backgroundColor', 'clear'];
+  let funcNames = ['bold', 'italic', 'underline', 'link', 'code', 'letterColor', 'backgroundColor', 'clear'];
 
   for (let name of funcNames) {
     let container = document.createElement('span');
-    container.style.position = 'relative';
-
     let img = document.createElement('img');
+    
+    container.style.position = 'relative';
 
     img.src = 'formatIcons/' + name + '.png';
     img.classList.add('formatBarImg');
@@ -110,31 +87,14 @@ createElem.onclick = function() {
     container.onclick = function(event) {
       if (event.target != img) return;
 
-      let elem = formatBarFuncs[name](container);
+      let result = formatBarFuncs[name](container);
 
-      if (elem) {
-        if (elem.wrapperElem) {
-          let {wrapperElem, submitButton, cancelField, attributes} = elem;
-
-          submitButton.addEventListener('click', function() {
-            if (submitButton.callEvent) {
-              for (let name of Object.keys(attributes)) {
-                wrapperElem[name] = attributes[name];
-              }
-            }
-          });
-          
-          cancelField.addEventListener('click', function() {
-            if (cancelField.callEvent) {
-              wrapperElem.replaceWith(wrapperElem.textContent);
-            }
-          });
-        } else {
-          wrapSelectedText(elem, textArea);
-        }
+      if (result) {
+        wrapSelectedText(result, textArea);
       }
     }
   }
+
 
   let textColor = formatBar.querySelector('[src*="letterColor"]').parentElement;
   let backgroundColor = formatBar.querySelector('[src*="backgroundColor"]').parentElement;
@@ -174,7 +134,6 @@ class FormatBar {
     this.textArea = textArea;
     this.textCol = textColor;
     this.backgroundCol = backgroundColor;
-    this.arr;
   }
 
   bold() {
@@ -207,14 +166,11 @@ class FormatBar {
     submitButton.style.padding = ' 0.7% 6% 0.7%';
     win.winContent.style.marginBottom = '1px';
 
-    // if true call final handler
-    submitButton.callEvent = false;
-    win.darkLayer.callEvent = false;
-
     win.align();
 
     let a = document.createElement('a');
 
+    a.target = '_blank';
     wrapSelectedText(a, this.textArea);
 
     submitButton.addEventListener('click', function() {
@@ -237,17 +193,121 @@ class FormatBar {
 
     win.darkLayer.onclick = () => {
       win.delWindow();
-      win.darkLayer.callEvent = true;
+      a.replaceWith(a.textContent);
     }
 
-    return {
-      wrapperElem: a,
-      submitButton: submitButton,
-      cancelField: win.darkLayer,
-      attributes: {
-        target: '_blank'
+    return false;
+  }
+
+  code() {
+    let win = new ModalWin();
+    let textArea = this.textArea;
+
+    win.createWindow(60, false, 'white', true);
+
+    let code = document.createElement('div');
+    let result = document.createElement('div');
+    let codeContainer = document.createElement('div');
+
+    codeContainer.className = 'code-container';
+    code.contentEditable = true;
+
+    code.style.marginBottom = '0px';
+    code.style.float = 'left';
+
+    let exclamation = document.createElement('img');
+    let exclamationText = document.createElement('span');
+
+    exclamationText.className = 'exclamation-text';
+
+    exclamation.hidden = true;
+    exclamation.className = 'exclamation';
+    exclamation.src = 'icons/warning.png';
+
+    codeContainer.append(exclamation);
+    codeContainer.append(exclamationText);
+
+    win.changeFontSize(exclamationText, 7);
+
+    exclamationText.style.border = 'none';
+    exclamationText.style.outline = 'none';
+    exclamationText.style.boxShadow = 'none';
+
+    code.oninput = function() {
+      let cleanResult = cleanHTML(code.textContent);
+
+      result.innerHTML = cleanResult.html;
+      
+      if (cleanResult.hasBannedTags || cleanResult.hasBannedAttrs) {
+        if (exclamation.hidden) {
+          exclamation.hidden = false;
+          exclamation.classList.add('shake');
+
+          setTimeout(() => {
+            exclamation.classList.remove('shake');
+          }, 200);
+        }
+
+        if (cleanResult.hasBannedTags && !cleanResult.hasBannedAttrs) {
+          animateText(exclamation, exclamationText, 'Вы используете запрещенный тег! Такие теги игнорируются');
+        } else if (!cleanResult.hasBannedTags && cleanResult.hasBannedAttrs) {
+          animateText(exclamation, exclamationText, 'Вы используете тег с запрещенным атрибутом! Такие теги игнорируются');
+        } else {
+          animateText(exclamation, exclamationText, 'Вы используете запрещенный тег и тег с запрещенным атрибутом! Такие теги игнорируются', 5);
+        }
+      } else {
+        exclamation.hidden = true;
       }
     }
+
+    code.className = 'code pretty-scroll';
+    result.className = 'code pretty-scroll';
+
+    result.style.float = 'right';
+
+    codeContainer.append(code);
+    codeContainer.append(result);
+
+    win.addElement(codeContainer, 4);
+
+    let submitButton = win.submitButton('ок');
+
+    submitButton.style.padding = ' 0.7% 6% 0.7%';
+    win.winContent.style.marginBottom = '1px';
+
+    win.align();
+
+    submitButton.addEventListener('click', function() {
+      if (code.textContent) {
+        let codeElem = document.createElement('div');
+
+        if (textArea.querySelector('#placeholder')) {
+          textArea.innerHTML = '';
+        }
+
+        codeElem.innerHTML = result.innerHTML;
+        textArea.append(codeElem);
+
+        win.delWindow();
+      } else {
+        code.classList.add('shake');
+        code.style.boxShadow = `1px 1px 2px rgba(255, 0, 0, 0.366),
+        -1px 1px 2px rgba(255, 0, 0, 0.366),
+        1px -1px 2px rgba(255, 0, 0, 0.366),
+        -1px -1px 2px rgba(255, 0, 0, 0.366)`;
+
+        setTimeout(() => {
+          code.classList.remove('shake');
+          code.style.boxShadow = '';
+        }, 200);
+      }
+    });
+
+    win.darkLayer.onclick = () => {
+      win.delWindow();
+    }
+
+    return false;
   }
 
   letterColor() {
@@ -348,4 +408,92 @@ function getCoords(elem) {
     bottom: box.bottom + window.pageYOffset,
     left: box.left + window.pageXOffset
   };
+}
+
+function cleanHTML(html) {
+  let newDocument = document.implementation.createHTMLDocument('New Document');
+  let container = newDocument.createElement('div');
+  let bannedTags = ['link', 'style', 'script', 'dialog'];
+  let bannedAttrs = ['id', 'class', 'style'];
+  let hasBannedTags = false;
+  let hasBannedAttrs = false;
+
+  container.innerHTML = html;
+  newDocument.body.append(container);
+
+  let allNested = newDocument.querySelectorAll('*');
+
+  for (let elem of allNested) {
+    if (bannedTags.includes(elem.tagName.toLowerCase())) {
+      let cleanInner = cleanHTML(elem.innerHTML);
+
+      hasBannedTags = true;
+      elem.outerHTML = cleanInner.html;
+      hasBannedAttrs = cleanInner.hasBannedAttrs;
+
+      continue;
+    }
+
+    for (let attr of elem.attributes) {
+      if (attr.name.startsWith('on') || bannedAttrs.includes(attr.name.toLowerCase())) {
+        let cleanInner = cleanHTML(elem.innerHTML);
+
+        hasBannedAttrs = true;
+        elem.outerHTML = cleanInner.html;
+        hasBannedTags = cleanInner.hasBannedTags;
+
+        break;
+      }
+    }
+  }
+
+  return {
+    html: container.innerHTML,
+    hasBannedTags: hasBannedTags,
+    hasBannedAttrs: hasBannedAttrs
+  };
+}
+
+function animateText(hoverElem, textElem, text, delay=10) {
+  let typingId;
+  let backspacingId;
+  let index;
+
+  hoverElem.onmouseover = function() {
+    clearInterval(backspacingId);
+    index = textElem.textContent.length;
+
+    textElem.style.border = '';
+    textElem.style.outline = '';
+    textElem.style.boxShadow = '';
+
+    typingId = setInterval(() => {
+      let content = text[index];
+      
+      if (content) {
+        textElem.textContent += content;
+      }
+      
+      index++;
+      if (index == text.length) {
+        clearInterval(typingId);
+      }
+    }, delay);
+  }
+
+  hoverElem.onmouseout = function() {
+    clearInterval(typingId);
+    
+    backspacingId = setInterval(() => {
+      if (!textElem.textContent.length) {
+        textElem.style.border = 'none';
+        textElem.style.outline = 'none';
+        textElem.style.boxShadow = 'none';
+
+        clearInterval(backspacingId);
+      }
+
+      textElem.textContent = textElem.textContent.slice(0, textElem.textContent.length - 1);
+    }, delay);
+  }
 }
